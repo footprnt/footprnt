c286b5f5438a6bc6a8a7726f05c9479cea6439fb
jocelynshen <jocelyn.j.shen@gmail.com>
2019-07-11 12:35:40 -0700

    logout working

--- app/src/main/java/com/example/footprnt/ProfileFragment.java
+++ app/src/main/java/com/example/footprnt/ProfileFragment.java
@@ -1,18 +1,44 @@
 package com.example.footprnt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.parse.ParseUser;
 
 public class ProfileFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        final ImageView settings = v.findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(getActivity(), settings);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        ParseUser.logOut();
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                        return true;
+                    }
+                });
+                popup.show();//showing popup menu
+            }
+        });
+        return v;
     }
 
     @Override
