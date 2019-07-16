4f658b32a6e3bee0c7e6b4696f30771f1ab1c897
ajankenm <ajankenmann@gmail.com>
2019-07-16 11:11:29 -0700

    Android Code Style TODOs

--- app/src/main/java/com/example/footprnt/DiscoverFragment.java
+++ app/src/main/java/com/example/footprnt/DiscoverFragment.java
@@ -8,13 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DiscoverFragment extends Fragment {
+// TODO: Javadoc
+public class DiscoverFragment extends Fragment { // TODO: newline between class definition and first line of code
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_discover, container, false);
     }
 
+    // TODO: method does nothing, delete
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
