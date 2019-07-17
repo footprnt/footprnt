8806893383a4c73c1c73ac67d5074fcb5f82ad9a
Clarisa Leu <clarisaleu@gmail.com>
2019-07-17 11:50:14 -0700

    add update stats method

--- app/src/main/java/com/example/footprnt/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/footprnt/Profile/ProfileFragment.java
@@ -1,8 +1,11 @@
 package com.example.footprnt.Profile;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -26,13 +29,17 @@ import com.bumptech.glide.Glide;
 import com.example.footprnt.MainActivity;
 import com.example.footprnt.Models.Post;
 import com.example.footprnt.R;
+import com.google.android.gms.maps.model.LatLng;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -49,6 +56,7 @@ public class ProfileFragment extends Fragment {
     HashMap<String, Integer> cities;
     HashMap<String, Integer> countries;
     HashMap<String, Integer> continents;
+    ArrayList<HashMap<String,Integer>> statsList;
 
 
     // For post feed:
@@ -85,7 +93,7 @@ public class ProfileFragment extends Fragment {
 
         // For profile image:
         ivProfileImage = v.findViewById(R.id.ivProfileImageMain);
-        if(ParseUser.getCurrentUser().getParseFile("profileImg")!=null) {
+        if (ParseUser.getCurrentUser().getParseFile("profileImg") != null) {
             Glide.with(getContext()).load(ParseUser.getCurrentUser().getParseFile("profileImg").getUrl()).into(ivProfileImage);
         } else {
             Glide.with(getContext()).load(R.drawable.ic_user).into(ivProfileImage);
@@ -115,22 +123,36 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        // For stat view
-        lvStats = v.findViewById(R.id.lvStatKey);
-        stats = new ArrayList<>();
-        getStats();
-        statAdapter = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_list_item_1, stats);
-        lvStats.setAdapter(statAdapter);
+
+        // Populate stat maps and get posts
+        posts = new ArrayList<>();
+        cities = new HashMap<>();
+        countries = new HashMap<>();
+        continents = new HashMap<>();
+        getPosts();
+        Log.e(TAG, String.format("Posts: %s, Cities: %s, Countries: %s, Continents: %s",posts.size(), cities.size(), countries.size(), continents.size()));
+
+        // Populate statsList
+        statsList  = new ArrayList<>();
+        statsList.add(cities);
+        statsList.add(countries);
+        statsList.add(continents);
+        updateStats();
 
         // For post feed view:
-        posts = new ArrayList<>();
-        getPosts();
         postAdapter = new PostAdapter(posts);
         rvPosts = v.findViewById(R.id.rvFeed);
-        rvPosts.setLayoutManager(new GridLayoutManager(v.getContext(),3));
+        rvPosts.setLayoutManager(new GridLayoutManager(v.getContext(), 3));
         rvPosts.setNestedScrollingEnabled(false);
         rvPosts.setHasFixedSize(true);
         rvPosts.setAdapter(postAdapter);
+
+        // For stat view
+        lvStats = v.findViewById(R.id.lvStatKey);
+        //stats = new ArrayList<>();
+
+        //statAdapter = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_list_item_1, stats);
+        //lvStats.setAdapter(statAdapter);
 
 
         // Refresh listener for post feed
@@ -143,12 +165,11 @@ public class ProfileFragment extends Fragment {
         });
 
 
-
         return v;
     }
 
     // Get posts
-    private void getPosts(){
+    private void getPosts() {
         final Post.Query postsQuery = new Post.Query();
         postsQuery
                 .getTop()
@@ -160,12 +181,36 @@ public class ProfileFragment extends Fragment {
             public void done(List<Post> objects, ParseException e) {
                 if (e == null) {
                     for (int i = 0; i < objects.size(); i++) {
-                        Post post = (Post) objects.get(i);
+                        final Post post = (Post) objects.get(i);
                         // Only add current user's posts
-                        if(post.getUser().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())) {
+                        if (post.getUser().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())) {
                             posts.add(post);
                             postAdapter.notifyItemInserted(posts.size() - 1);
-                            
+                            // Get post stats and update user stats
+                            LatLng pt = new LatLng(post.getLocation().getLatitude(), post.getLocation().getLongitude());
+                            ArrayList<String> postStats = getAddress(getContext(), pt);
+                            // Fill HashMaps
+                            // Cities
+                            if (!cities.containsKey(postStats.get(0))) {
+                                // User first visit
+                                cities.put(postStats.get(0), 1);
+                            } else {
+                                // User already visited, increment count
+                                cities.put(postStats.get(0), cities.get(postStats.get(0)) + 1);
+                            }
+                            // Countries
+                            if (!countries.containsKey(postStats.get(1))) {
+                                countries.put(postStats.get(1), 1);
+                            } else {
+                                countries.put(postStats.get(1), countries.get(postStats.get(1)) + 1);
+                            }
+                            // Continents
+                            if (!continents.containsKey(postStats.get(2))) {
+                                continents.put(postStats.get(2), 1);
+                            } else {
+                                continents.put(postStats.get(2), continents.get(postStats.get(2)) + 1);
+                            }
+
                         }
                     }
                     swipeContainer.setRefreshing(false);
@@ -177,26 +222,17 @@ public class ProfileFragment extends Fragment {
     }
 
 
-    private void getStats(){
-        final ParseUser user = ParseUser.getCurrentUser();
-        ArrayList<ArrayList<String>> list = (ArrayList<ArrayList<String>>) user.get("stats");
-        if(list!=null){
-            for (ArrayList<String> stat : list) {
-                String statKey=null;
-                String statNum;
-                for(int i = 0 ; i < stat.size(); i++){
-                    if (i%2==0) {
-                        // Get key
-                        statKey = stat.get(i);
-                    } else {
-                        // Get value
-                        statNum = stat.get(i);
-                        stats.add(statKey + "  " + statNum);
-                    }
-
-                }
-            }
+    private void updateStats() {
+        ArrayList<ArrayList<String>> res = new ArrayList<>();
+        for(int i = 0; i < statsList.size(); i++ ){  // Loop through number of stats we're tracking
+            HashMap<String, Integer> innerList = statsList.get(i);
+            ArrayList<String> toAdd = new ArrayList<>();
+            toAdd.add(String.format("%s",innerList.size()));
+            res.add(toAdd);
         }
+        final ParseUser user = ParseUser.getCurrentUser();
+        user.put("stats", res);
+        user.saveInBackground();
     }
 
 
@@ -208,10 +244,39 @@ public class ProfileFragment extends Fragment {
     }
 
     // Helper method to handle errors, log them, and alert user
-    private void logError(String message, Throwable error, boolean alertUser){
+    private void logError(String message, Throwable error, boolean alertUser) {
         Log.e(TAG, message, error);
-        if(alertUser){
+        if (alertUser) {
             Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    // Returns ArrayList<String> of the [City, Country, and Continent] of point
+    public ArrayList<String> getAddress(Context context, LatLng point) {
+        try {
+            String json_str = "{\"AD\":\"Europe\",\"AE\":\"Asia\",\"AF\":\"Asia\",\"AG\":\"North America\",\"AI\":\"North America\",\"AL\":\"Europe\",\"AM\":\"Asia\",\"AN\":\"North America\",\"AO\":\"Africa\",\"AQ\":\"Antarctica\",\"AR\":\"South America\",\"AS\":\"Australia\",\"AT\":\"Europe\",\"AU\":\"Australia\",\"AW\":\"North America\",\"AZ\":\"Asia\",\"BA\":\"Europe\",\"BB\":\"North America\",\"BD\":\"Asia\",\"BE\":\"Europe\",\"BF\":\"Africa\",\"BG\":\"Europe\",\"BH\":\"Asia\",\"BI\":\"Africa\",\"BJ\":\"Africa\",\"BM\":\"North America\",\"BN\":\"Asia\",\"BO\":\"South America\",\"BR\":\"South America\",\"BS\":\"North America\",\"BT\":\"Asia\",\"BW\":\"Africa\",\"BY\":\"Europe\",\"BZ\":\"North America\",\"CA\":\"North America\",\"CC\":\"Asia\",\"CD\":\"Africa\",\"CF\":\"Africa\",\"CG\":\"Africa\",\"CH\":\"Europe\",\"CI\":\"Africa\",\"CK\":\"Australia\",\"CL\":\"South America\",\"CM\":\"Africa\",\"CN\":\"Asia\",\"CO\":\"South America\",\"CR\":\"North America\",\"CU\":\"North America\",\"CV\":\"Africa\",\"CX\":\"Asia\",\"CY\":\"Asia\",\"CZ\":\"Europe\",\"DE\":\"Europe\",\"DJ\":\"Africa\",\"DK\":\"Europe\",\"DM\":\"North America\",\"DO\":\"North America\",\"DZ\":\"Africa\",\"EC\":\"South America\",\"EE\":\"Europe\",\"EG\":\"Africa\",\"EH\":\"Africa\",\"ER\":\"Africa\",\"ES\":\"Europe\",\"ET\":\"Africa\",\"FI\":\"Europe\",\"FJ\":\"Australia\",\"FK\":\"South America\",\"FM\":\"Australia\",\"FO\":\"Europe\",\"FR\":\"Europe\",\"GA\":\"Africa\",\"GB\":\"Europe\",\"GD\":\"North America\",\"GE\":\"Asia\",\"GF\":\"South America\",\"GG\":\"Europe\",\"GH\":\"Africa\",\"GI\":\"Europe\",\"GL\":\"North America\",\"GM\":\"Africa\",\"GN\":\"Africa\",\"GP\":\"North America\",\"GQ\":\"Africa\",\"GR\":\"Europe\",\"GS\":\"Antarctica\",\"GT\":\"North America\",\"GU\":\"Australia\",\"GW\":\"Africa\",\"GY\":\"South America\",\"HK\":\"Asia\",\"HN\":\"North America\",\"HR\":\"Europe\",\"HT\":\"North America\",\"HU\":\"Europe\",\"ID\":\"Asia\",\"IE\":\"Europe\",\"IL\":\"Asia\",\"IM\":\"Europe\",\"IN\":\"Asia\",\"IO\":\"Asia\",\"IQ\":\"Asia\",\"IR\":\"Asia\",\"IS\":\"Europe\",\"IT\":\"Europe\",\"JE\":\"Europe\",\"JM\":\"North America\",\"JO\":\"Asia\",\"JP\":\"Asia\",\"KE\":\"Africa\",\"KG\":\"Asia\",\"KH\":\"Asia\",\"KI\":\"Australia\",\"KM\":\"Africa\",\"KN\":\"North America\",\"KP\":\"Asia\",\"KR\":\"Asia\",\"KW\":\"Asia\",\"KY\":\"North America\",\"KZ\":\"Asia\",\"LA\":\"Asia\",\"LB\":\"Asia\",\"LC\":\"North America\",\"LI\":\"Europe\",\"LK\":\"Asia\",\"LR\":\"Africa\",\"LS\":\"Africa\",\"LT\":\"Europe\",\"LU\":\"Europe\",\"LV\":\"Europe\",\"LY\":\"Africa\",\"MA\":\"Africa\",\"MC\":\"Europe\",\"MD\":\"Europe\",\"ME\":\"Europe\",\"MG\":\"Africa\",\"MH\":\"Australia\",\"MK\":\"Europe\",\"ML\":\"Africa\",\"MM\":\"Asia\",\"MN\":\"Asia\",\"MO\":\"Asia\",\"MP\":\"Australia\",\"MQ\":\"North America\",\"MR\":\"Africa\",\"MS\":\"North America\",\"MT\":\"Europe\",\"MU\":\"Africa\",\"MV\":\"Asia\",\"MW\":\"Africa\",\"MX\":\"North America\",\"MY\":\"Asia\",\"MZ\":\"Africa\",\"NA\":\"Africa\",\"NC\":\"Australia\",\"NE\":\"Africa\",\"NF\":\"Australia\",\"NG\":\"Africa\",\"NI\":\"North America\",\"NL\":\"Europe\",\"NO\":\"Europe\",\"NP\":\"Asia\",\"NR\":\"Australia\",\"NU\":\"Australia\",\"NZ\":\"Australia\",\"OM\":\"Asia\",\"PA\":\"North America\",\"PE\":\"South America\",\"PF\":\"Australia\",\"PG\":\"Australia\",\"PH\":\"Asia\",\"PK\":\"Asia\",\"PL\":\"Europe\",\"PM\":\"North America\",\"PN\":\"Australia\",\"PR\":\"North America\",\"PS\":\"Asia\",\"PT\":\"Europe\",\"PW\":\"Australia\",\"PY\":\"South America\",\"QA\":\"Asia\",\"RE\":\"Africa\",\"RO\":\"Europe\",\"RS\":\"Europe\",\"RU\":\"Europe\",\"RW\":\"Africa\",\"SA\":\"Asia\",\"SB\":\"Australia\",\"SC\":\"Africa\",\"SD\":\"Africa\",\"SE\":\"Europe\",\"SG\":\"Asia\",\"SH\":\"Africa\",\"SI\":\"Europe\",\"SJ\":\"Europe\",\"SK\":\"Europe\",\"SL\":\"Africa\",\"SM\":\"Europe\",\"SN\":\"Africa\",\"SO\":\"Africa\",\"SR\":\"South America\",\"ST\":\"Africa\",\"SV\":\"North America\",\"SY\":\"Asia\",\"SZ\":\"Africa\",\"TC\":\"North America\",\"TD\":\"Africa\",\"TF\":\"Antarctica\",\"TG\":\"Africa\",\"TH\":\"Asia\",\"TJ\":\"Asia\",\"TK\":\"Australia\",\"TM\":\"Asia\",\"TN\":\"Africa\",\"TO\":\"Australia\",\"TR\":\"Asia\",\"TT\":\"North America\",\"TV\":\"Australia\",\"TW\":\"Asia\",\"TZ\":\"Africa\",\"UA\":\"Europe\",\"UG\":\"Africa\",\"US\":\"North America\",\"UY\":\"South America\",\"UZ\":\"Asia\",\"VC\":\"North America\",\"VE\":\"South America\",\"VG\":\"North America\",\"VI\":\"North America\",\"VN\":\"Asia\",\"VU\":\"Australia\",\"WF\":\"Australia\",\"WS\":\"Australia\",\"YE\":\"Asia\",\"YT\":\"Africa\",\"ZA\":\"Africa\",\"ZM\":\"Africa\",\"ZW\":\"Africa\"}";
+            JSONObject jsonObject = new JSONObject(json_str);
+            Geocoder geo = new Geocoder(context, Locale.getDefault());
+            List<Address> addresses = geo.getFromLocation(point.latitude, point.longitude, 1);
+            if (addresses.isEmpty()) {
+                return new ArrayList<>();
+            } else {
+                if (addresses.size() > 0) {
+                    ArrayList<String> res = new ArrayList<>();
+                    String city = addresses.get(0).getLocality();
+                    res.add(city);
+                    String country = addresses.get(0).getCountryName();
+                    res.add(country);
+                    String continent = jsonObject.getString(addresses.get(0).getCountryCode());
+                    res.add(continent);
+                    return res;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // getFromLocation() may sometimes fail
+            return null;
+        }
+        return null;
     }
 }
