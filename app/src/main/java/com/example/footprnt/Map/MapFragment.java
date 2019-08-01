2abf7947a6533f42ba22c670cfa87f660ffe2e36
jocelynshen <jocelyn.j.shen@gmail.com>
2019-08-01 10:57:31 -0700

    prevent video upload

--- app/src/main/java/com/example/footprnt/Map/MapFragment.java
+++ app/src/main/java/com/example/footprnt/Map/MapFragment.java
@@ -220,35 +220,40 @@ public class MapFragment extends Fragment implements GoogleMap.OnMapLongClickLis
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, final Intent data) {
-        if (requestCode == AppConstants.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == getActivity().RESULT_OK) {
-                Bitmap takenImage = BitmapFactory.decodeFile(mPhotoFile.getAbsolutePath());
-                mImage.setVisibility(View.VISIBLE);
-                mImage.setImageBitmap(takenImage);
-                File photoFile = mHelper.getPhotoFileUri(getContext(), AppConstants.photoFileName);
-                mParseFile = new ParseFile(photoFile);
-            } else {
-                mParseFile = null;
-                Toast.makeText(getContext(), R.string.camera_message, Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            if (resultCode == getActivity().RESULT_OK) {
-                Bitmap bitmap = null;
-                Uri selectedImage = data.getData();
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
-                } catch (Exception e) {
+        try {
+            if (requestCode == AppConstants.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+                if (resultCode == getActivity().RESULT_OK) {
+                    Bitmap takenImage = BitmapFactory.decodeFile(mPhotoFile.getAbsolutePath());
+                    mImage.setVisibility(View.VISIBLE);
+                    mImage.setImageBitmap(takenImage);
+                    File photoFile = mHelper.getPhotoFileUri(getContext(), AppConstants.photoFileName);
+                    mParseFile = new ParseFile(photoFile);
+                } else {
+                    mParseFile = null;
+                    Toast.makeText(getContext(), R.string.camera_message, Toast.LENGTH_SHORT).show();
                 }
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, AppConstants.captureImageQuality, stream);
-                byte[] image = stream.toByteArray();
-                mParseFile = new ParseFile(AppConstants.imagePath, image);
-                final Bitmap finalBitmap = bitmap;
-                mImage.setVisibility(View.VISIBLE);
-                Glide.with(this).load(finalBitmap).into(mImage);
             } else {
-                mParseFile = null;
+                if (resultCode == getActivity().RESULT_OK) {
+                    Bitmap bitmap = null;
+                    Uri selectedImage = data.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
+                    } catch (Exception e) {
+                    }
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, AppConstants.captureImageQuality, stream);
+                    byte[] image = stream.toByteArray();
+                    mParseFile = new ParseFile(AppConstants.imagePath, image);
+                    final Bitmap finalBitmap = bitmap;
+                    mImage.setVisibility(View.VISIBLE);
+                    Glide.with(this).load(finalBitmap).into(mImage);
+                } else {
+                    mParseFile = null;
+                }
             }
+        } catch (Exception e){
+            Toast.makeText(getContext(), "Invalid media type", Toast.LENGTH_LONG).show();
+
         }
     }
 
