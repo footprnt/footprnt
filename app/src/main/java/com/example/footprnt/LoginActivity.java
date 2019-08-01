f04bb3c384becf2e21e0deeca4e39776ecb69688
Clarisa Leu <clarisaleu@gmail.com>
2019-07-31 20:46:00 -0700

    signup with facebook donnne

--- app/src/main/java/com/example/footprnt/LoginActivity.java
+++ app/src/main/java/com/example/footprnt/LoginActivity.java
@@ -7,23 +7,38 @@
 package com.example.footprnt;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.footprnt.Util.AppConstants;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.facebook.ParseFacebookUtils;
 
-import java.util.Arrays;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Handles all login activity
@@ -34,18 +49,25 @@ import java.util.List;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private final String TAG = "LoginActivity";
     private EditText mUsernameInput;
     private EditText mPasswordInput;
     private TextView mForgotPassword;
     private Button mLoginBtn;
     private Button mSignUpBtn;
     private Button mFacebookLoginBtn;
-    private final List<String> permissions = Arrays.asList(AppConstants.PUBLIC_PROFILE, AppConstants.email);
+    private String fullName;
+    private String profilePicId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         mUsernameInput = findViewById(R.id.username);
         mForgotPassword = findViewById(R.id.forgotPassword);
@@ -86,34 +108,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        mFacebookLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginFB();
-            }
-        });
-    }
 
-    /**
-     * Helper method to login/connect with Facebook
-     */
-    private void loginFB() {
-        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException err) {
-                if (user == null) {
-                    Toast.makeText(getApplicationContext(), "Error logging in with Facebook", Toast.LENGTH_LONG).show();
-                } else if (user.isNew()) {
-                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-                } else {
-                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        setUpFacebookLogin();
     }
 
     /**
@@ -137,6 +133,80 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // Required for making Facebook login work
         ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
     }
+
+    private void setUpFacebookLogin() {
+        mFacebookLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> permissions = new ArrayList();
+                permissions.add("email");
+                permissions.add("public_profile");
+                ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this,
+                        permissions, new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException err) {
+                                if (err != null) {
+                                    Log.d(TAG, "Error occurred" + err.toString());
+                                    err.printStackTrace();
+                                } else if (user == null) {
+                                    Log.d(TAG, "The user cancelled the Facebook login.");
+                                } else {
+                                    handleValidUser(user);
+                                }
+                            }
+                        }
+                );
+            }
+        });
+    }
+
+    private void handleValidUser(final ParseUser user) {
+        if (user.isNew()) {
+            GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+                            try {
+                                fullName = String.valueOf(object.getString("name"));
+                                user.put("username", fullName);
+                                profilePicId = String.valueOf(object.getString("id"));
+
+                                URL picUrl = new URL("https://graph.facebook.com/" + Profile.getCurrentProfile().getId() + "/picture?type=large");
+                                Bitmap bitmap = BitmapFactory.decodeStream(picUrl.openConnection().getInputStream());
+                                ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+                                bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+                                byte[] imageByte = byteArrayOutputStream.toByteArray();
+                                ParseFile parseFile = new ParseFile("image_file.JPEG",imageByte);
+                                if(parseFile!=null){
+                                    user.put("profileImg", parseFile);
+                                }
+
+                                user.saveInBackground(new SaveCallback() {
+                                    @Override
+                                    public void done(ParseException e) {
+                                        startActivity(new Intent(LoginActivity.this,
+                                                HomeActivity.class));
+                                    }
+                                });
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    });
+            request.executeAsync();
+        } else {
+            startActivity(new Intent(LoginActivity.this,
+                    HomeActivity.class));
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/footprnt/SignUpActivity.java
+++ app/src/main/java/com/example/footprnt/SignUpActivity.java
@@ -6,7 +6,9 @@
  */
 package com.example.footprnt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +17,20 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.footprnt.Util.AppConstants;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * Handles all sign up activities
@@ -27,6 +41,7 @@ import com.parse.SignUpCallback;
  */
 public class SignUpActivity extends AppCompatActivity {
 
+    private final String TAG = "SignUpActivity";
     private EditText mUsernameInput;
     private EditText mPasswordInput;
     private EditText mPasswordConfirm;
@@ -34,11 +49,17 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText mEmailInput;
     private EditText mDescription;
     private Button mSubmitNewUser;
+    private String fullName;
+    private ParseUser mUser;
+    private String profilePicId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        getFacebookInformation();
+        validRegistrationCheck();
+
 
         mUsernameInput = findViewById(R.id.new_username);
         mPasswordInput = findViewById(R.id.new_password);
@@ -82,4 +103,75 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+    //checks to see if information user inputted is valid
+    private void validRegistrationCheck() {
+        final String username = mUsernameInput.getText().toString();
+        if (username.isEmpty()) {
+            Toast.makeText(SignUpActivity.this, "Username empty", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            queryUsernameExists(username);
+        }
+    }
+
+    //gets user's full name and profile picture from Facebook
+    private void getFacebookInformation() {
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            fullName = String.valueOf(object.getString("name"));
+                            profilePicId = String.valueOf(object.getString("id"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+        request.executeAsync();
+    }
+
+    //completes registration process if new user from facebook login
+    private void completeRegister(ParseUser newUser, final String username) {
+        newUser.setUsername(username);
+        newUser.put("profileImg", profilePicId);
+        newUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Helper method to see if user name exists already in Parse DB
+     * @param username to query
+     */
+    private void queryUsernameExists(final String username) {
+        ParseQuery<ParseUser> query = getUserByUsername(username);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e != null) {
+                    Log.d(TAG, "error querying for username");
+                    e.printStackTrace();
+                } else if (objects.size() != 0){
+                    Toast.makeText(SignUpActivity.this, "Username taken", Toast.LENGTH_LONG).show();
+                } else {
+                    completeRegister(ParseUser.getCurrentUser(), username);
+                }
+            }
+        });
+    }
+
+    // Query for users that have the given username.
+    public static ParseQuery<ParseUser> getUserByUsername(String username) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("username", username);
+        return query;
+    }
+
 }
