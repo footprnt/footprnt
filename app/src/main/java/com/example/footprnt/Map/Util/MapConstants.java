d6ee49639b3f9959626961d1c4e6f51f8da31c10
Stanley Nwakamma <stanleynwa@fb.com>
2019-08-08 14:50:54 -0700

    changed to 50 miles radius

--- app/src/main/java/com/example/footprnt/Map/Util/MapConstants.java
+++ app/src/main/java/com/example/footprnt/Map/Util/MapConstants.java
@@ -22,7 +22,7 @@ public class MapConstants {
     public static String FASHION = "fashion";
     public static String TRAVEL = "travel";
     public static String NATURE = "nature";
-    public static int POST_RADIUS = 10;
+    public static int POST_RADIUS = 50;
 
     // Map Styles:
     public static String MAP_STYLE = "map_style";
